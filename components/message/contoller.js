048bc2ac9443a579188597b03f48124764ed2d4f
Camilo Davila <camilo7davila@gmail.com>
2020-03-11 11:33:58 -0500

    version con direccion de foto

--- components/message/contoller.js
+++ components/message/contoller.js
@@ -9,7 +9,7 @@ function addMessage(chat, user, message, file) {
 
         let fileUrl = '';
         if(file){
-            fileUrl = 'http://localhost:3000/app/files/'+file.filename
+            fileUrl = 'https://telegramnodejs.herokuapp.com/app/files/'+file.filename
         }
 
         const fullMessage ={
